eb483202ab0aac6a1137c41df84c308c53ce6241
wenhuang1 <huangwen@login.khoury.neu.edu>
2024-04-12 12:32:35 -0400

    Change randomPredictor to historyPredictor

--- predictor/predictor.py
+++ predictor/predictor.py
@@ -13,13 +13,13 @@ def aiPredictor(asset_returns: np.ndarray, asset_std: np.ndarray, factor_data: n
     """
     pass
 
-def randomPredictor(asset_returns: np.ndarray, asset_std: np.ndarray, factor_data: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+def historyPredictor(asset_returns: np.ndarray, asset_std: np.ndarray, factor_data: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
-    Randomly predict asset monthly expected return, standard deviation, and covariance using Bootstrap.
+    Predict asset monthly expected return, standard deviation, and covariance using history rolling average data.
 
     :param asset_returns: NumPy array of asset monthly returns.
     :param asset_std: NumPy array of asset monthly standard deviations.
     :param factor_data: NumPy array of factor data.
-    :return: A tuple of NumPy arrays containing random predicted monthly expected returns, standard deviations, and covariance matrix.
+    :return: A tuple of NumPy arrays containing rolling average monthly expected returns, standard deviations, and covariance matrix.
     """
     pass
