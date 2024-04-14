98bc4132a5a28d733cb0bd9ab1da3a744ae031a4
Zhongshi Luo <bdssrojack@gmail.com>
2024-04-14 19:06:12 -0400

    GARCH implemented

--- predictor/predictor.py
+++ predictor/predictor.py
@@ -1,7 +1,18 @@
+from datetime import timedelta
 import numpy as np
 import pandas as pd
+import itertools
+from arch import arch_model
+import matplotlib.pyplot as plt
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from typing import Tuple, Dict
 from models import Asset, Factor
+from pomegranate import BayesianNetwork
+import warnings
+
+warnings.filterwarnings('ignore')
+
+window_size = 36
 
 def aiPredictor(asset_returns: np.ndarray, asset_std: np.ndarray, factor_data: np.ndarray) -> Tuple[int, int]:
     """
@@ -12,7 +23,86 @@ def aiPredictor(asset_returns: np.ndarray, asset_std: np.ndarray, factor_data: n
     :param factor_data: NumPy array of factor data.
     :return: predict return, predict std
     """
-    pass
+    # asset_returns = np.nan_to_num(asset_returns)
+    # print("Minimum:", np.min(asset_returns))
+    # print("Maximum:", np.max(asset_returns))
+    # print("Mean:", np.mean(asset_returns))
+    # print("Standard Deviation:", np.std(asset_returns))
+
+    # Plot asset returns and standard deviation
+    plt.figure(figsize=(10, 6))
+    plt.plot(asset_std, label='Asset Standard Deviation')
+    plt.plot(asset_returns, label='Asset Returns')
+    plt.legend(['Asset Standard Deviation', 'Asset Returns'])
+    plt.title('Asset Returns and Standard Deviation')
+
+    # Plot PACF
+    plot_pacf(asset_returns**2)
+
+    # Fit GARCH model
+    # best_aic = np.inf
+    best_p, best_q = 1, 1
+    # for p, q in itertools.product(range(1, 4), repeat=2):
+    #     try:
+    #         temp_model = arch_model(asset_returns, p=p, q=q)
+    #         temp_model_fit = temp_model.fit(disp='off')
+    #         if temp_model_fit.aic < best_aic:
+    #             best_aic = temp_model_fit.aic
+    #             best_p, best_q = p, q
+    #     except ValueError:
+    #         continue
+    
+    model = arch_model(asset_returns, p=best_p, q=best_q)
+    model_fit = model.fit()
+    print(model_fit.summary())
+
+    # Scale factor
+    scale_factor = 1e+18
+    scaled_returns = asset_returns * scale_factor
+
+    # Rolling forecast
+    rolling_predictions = []
+    if len(asset_returns) >= window_size:
+        for i in range(len(asset_returns) - window_size + 1):
+            train = asset_returns[i:i + window_size]
+            # train = scaled_returns[i:i + window_size]
+            model = arch_model(train, p=best_p, q=best_q)
+            model_fit = model.fit(disp='off')
+            pred = model_fit.forecast(horizon=1)
+            # rolling_predictions.append(np.sqrt(pred.variance.values[-1, :][0] / (scale_factor ** 2)))
+            rolling_predictions.append(np.sqrt(pred.variance.values[-1, :][0]))
+    
+    # Plot rolling predictions
+    plt.figure(figsize=(10, 6))
+    plt.plot(asset_returns, label='True Returns')
+    plt.plot(rolling_predictions, label='Predicted Volatility')
+    plt.plot(asset_std, label='True Volatility')
+    plt.title('Rolling Volatility Prediction')
+    plt.legend(['True Returns', 'Predicted Volatility', 'True Volatility'])
+
+    pred = model_fit.forecast(horizon=7)
+    pred = pd.Series(np.sqrt(pred.variance.values[-1, :]))
+    plt.figure(figsize=(10, 6))
+    plt.plot(pred)
+    plt.title('7-Day Volatility Forecast')
+
+    plt.show()
+
+    pred = model_fit.forecast(horizon=1)
+    pred_std = np.sqrt(pred.variance.values[-1, :][0])
+
+    # predict return
+    # error: normal distribution N(0, 1)
+    curr_return = np.random.normal(0, 1) * pred_std
+
+    # use bayesian network to predict return
+    structure = ((), (), (), (0, 1, 2))
+    model = BayesianNetwork.from_structure(factor_data, structure)
+    pred_return = model.predict_proba({})[2]
+
+
+    return (pred_return, pred_std)
+
 
 def historyPredictor(asset_returns: np.ndarray, asset_std: np.ndarray) -> Tuple[int, int]:
     """
@@ -24,7 +114,7 @@ def historyPredictor(asset_returns: np.ndarray, asset_std: np.ndarray) -> Tuple[
     :return: predict return, predict std
     """
 
-    window_size = 36
+    
     rolling_returns = np.convolve(asset_returns, np.ones(window_size)/window_size, 'valid')
     rolling_std_devs = np.convolve(asset_std, np.ones(window_size)/window_size, 'valid')
 
