932697d03e5f05971587fdd5b751dc1a7d204930
wenhuang1 <huangwen@login.khoury.neu.edu>
2024-04-15 11:17:24 -0400

    Add allocation plot and comparison

--- main.py
+++ main.py
@@ -9,13 +9,8 @@ import plot as plot
 
 OBSERVATION_PERIOD = 5 * 12
 
-MAX_WEIGHT = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-MIN_WEIGHT = [0, 0, 0, 0, 0, 0, 0]
-
-def _getCovariance(returns):
-    # This is a simplistic approach to compute covariance from returns.
-    # numpy's cov function computes covariance matrix.
-    return np.cov(returns, rowvar=False)
+MAX_WEIGHT = []
+MIN_WEIGHT = []
 
 def _getCAPMWeights(market_caps):
     total_market_cap = np.sum(market_caps, axis=0)
@@ -25,7 +20,8 @@ def _getCAPMWeights(market_caps):
 def historyPredictWithMeanVariance(allAssets, allFactors):
     length = len(next(iter(allAssets.values())).returns)
     start = int(length * 3 / 4)
-    returns = []
+    portfolio_returns = []
+    portfolio_std_devs = []
 
     # Iterate over each time point from start to the end of the dataset
     for i in range(start, length):
@@ -42,20 +38,61 @@ def historyPredictWithMeanVariance(allAssets, allFactors):
             current_returns.append(asset.returns[i])
 
         # Get covariance matrix from the returns up to the current time
-        cov_matrix = _getCovariance([asset.returns[i - OBSERVATION_PERIOD:i] for asset in allAssets.values()])
-        weights = allocator.meanVarianceAllocator(np.array(expected_returns), np.array(standard_deviations), cov_matrix)
+        cov_matrix = utils.calculate_cov(allAssets, OBSERVATION_PERIOD)
+
+        weights = allocator.meanVarianceAllocator(np.array(expected_returns), cov_matrix, riskFreeAssets.returns[i - 1])
+        print(weights)
         
         # Calculate portfolio return for the current month using dot product of weights and actual returns
         month_return = np.dot(weights, current_returns)
-        returns.append(month_return)
+        portfolio_returns.append(month_return)
+        month_std_dev = np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights)))
+        portfolio_std_devs.append(month_std_dev)
 
-    return np.array(returns)
+    return np.array(portfolio_returns), np.array(portfolio_std_devs)
+
+def historyPredictWithGeneticAllocation(allAssets, allFactors):
+    MAX_WEIGHT = np.array([1 for _ in range(len(allAssets))])
+    MIN_WEIGHT = np.array([0 for _ in range(len(allAssets))])
+    length = len(next(iter(allAssets.values())).returns)
+    start = int(length * 3 / 4)
+    portfolio_returns = []
+    portfolio_std_devs = []
+
+    # Iterate over each time point from start to the end of the dataset
+    for i in range(start, length):
+        expected_returns = []
+        standard_deviations = []
+        current_returns = []
+
+        # Collect data for all assets up to the current time point
+        for asset_name, asset in allAssets.items():
+            # Assume predictor.historyPredictor returns the expected return and std for the next period
+            cur_return, cur_std = predictor.historyPredictor(asset.returns[i - OBSERVATION_PERIOD:i], asset.stds[i - OBSERVATION_PERIOD:i])
+            expected_returns.append(cur_return)
+            standard_deviations.append(cur_std)
+            current_returns.append(asset.returns[i])
+
+        # Get covariance matrix from the returns up to the current time
+        cov_matrix = utils.calculate_cov(allAssets, OBSERVATION_PERIOD)
+        capm_weights = utils.calculate_market_weights(allAssets, i)
+        weights = allocator.aiAllocator(np.array(expected_returns), np.array(standard_deviations), cov_matrix, MIN_WEIGHT, MAX_WEIGHT, capm_weights, riskFreeAssets.returns[i - 1])
+        
+        # Calculate portfolio return for the current month using dot product of weights and actual returns
+        month_return = np.dot(weights, current_returns)
+        portfolio_returns.append(month_return)
+        month_std_dev = np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights)))
+        portfolio_std_devs.append(month_std_dev)
+
+    return np.array(portfolio_returns), np.array(portfolio_std_devs)
 
 
 def bayesianPredictWithGeneticAllocation(allAssets, allFactors):
     length = len(next(iter(allAssets.values())).returns)
     start = int(length * 3 / 4)
-    returns = []
+    portfolio_returns = []
+    portfolio_std_devs = []
+
 
     for i in range(start, length):
         expected_returns = []
@@ -70,32 +107,41 @@ def bayesianPredictWithGeneticAllocation(allAssets, allFactors):
             current_returns.append(asset.returns[i])
             market_caps.append(asset.market_cap[i - 1])
 
-        cov_matrix = _getCovariance([asset.returns[i - OBSERVATION_PERIOD:i] for asset in allAssets.values()])
-        market_caps = np.array(market_caps)
-        capm_weights = _getCAPMWeights(market_caps)
+        cov_matrix = utils.calculate_cov(allAssets, OBSERVATION_PERIOD)
+        capm_weights = utils.calculate_market_weights(allAssets, i)
         weights = allocator.aiAllocator(np.array(expected_returns), np.array(standard_deviations), cov_matrix, MIN_WEIGHT, MAX_WEIGHT, capm_weights)
         
         month_return = np.dot(weights, current_returns)
-        returns.append(month_return)
+        portfolio_returns.append(month_return)
+        month_std_dev = np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights)))
+        portfolio_std_devs.append(month_std_dev)
 
-    return np.array(returns)
+    return np.array(portfolio_returns), np.array(portfolio_std_devs)
 
 # Initialize allAssets and allFactors with the data from data_parser modules
 
-data_parser.fetch_and_save_all_data()
+# data_parser.fetch_and_save_all_data()
 allAssets = data_parser.readAssetDailyData()
+riskFreeAssets = data_parser.readRiskFreeData()
+returns_H_G, std_H_G = historyPredictWithGeneticAllocation(allAssets, None)
+returns_H_M, std_H_M = historyPredictWithMeanVariance(allAssets, None)
+
+plot.plot_returns_and_std(returns_H_M, std_H_M, returns_H_G, std_H_G)
+plot.plot_time_series(returns_H_M, std_H_M, returns_H_G, std_H_G)
+plot.plot_geometric_cumulative_returns(returns_H_M, returns_H_G)
 
 # Plot
 
-# avg_returns, avg_stds, cov_matrix = utils.calculate_averages_and_cov(allAssets)
+# cov_matrix = utils.calculate_cov(allAssets, OBSERVATION_PERIOD)
+# returns = [asset.returns[-1] for asset in allAssets.values()]
 # market_weights = utils.calculate_market_weights(allAssets)
-# plot.plot_mean_variance(avg_returns, cov_matrix)
-
+# plot.plot_mean_variance(returns, cov_matrix)
+# plot.plot_mean_variance_with_fitness(returns, cov_matrix, market_weights, MIN_WEIGHT, MAX_WEIGHT)
 
 # allFactors = data_parser.readFactorData()
 # portfolio_returns = historyPredictWithMeanVariance(allAssets, allFactors)
 # portfolio_returns_ai = bayesianPredictWithGeneticAllocation(allAssets, allFactors)
 
 # test aiPredictor
-cur_return, cur_std = predictor.aiPredictor(allAssets['SP500'].returns, allAssets['SP500'].stds, None)
-print(cur_return, cur_std)
+# cur_return, cur_std = predictor.aiPredictor(allAssets['SP500'].returns, allAssets['SP500'].stds, None)
+# print(cur_return, cur_std)
--- utils.py
+++ utils.py
@@ -3,44 +3,40 @@ import pandas as pd
 from datetime import datetime
 
 
-def calculate_market_weights(all_assets):
+def calculate_market_weights(all_assets, i):
     """ Calculate market weights based on the market cap. """
-    total_market_cap = sum(asset.market_cap[-1] for asset in all_assets.values())
-    return np.array([asset.market_cap[-1] / total_market_cap for asset in all_assets.values()])
+    total_market_cap = sum(asset.market_cap[i] for asset in all_assets.values())
+    return np.array([asset.market_cap[i] / total_market_cap for asset in all_assets.values()])
 
-
-def calculate_averages_and_cov(all_assets):
+def calculate_cov(all_assets, observation_period):
     """ Calculate average returns, standard deviations, and covariance matrix for the past 5 years. """
-    now = datetime.now()
-    past_date = now.replace(year=now.year - 5)
-    
-    avg_returns = []
-    avg_stds = []
     values = []
     
     for name, asset in all_assets.items():
         # Filter data to the last five years
-        recent_data = asset.returns[(asset.returns.index > past_date)]
-        avg_return = np.mean(recent_data)
-        avg_std = np.mean(asset.stds[(asset.stds.index > past_date)])
-        
-        avg_returns.append(avg_return)
-        avg_stds.append(avg_std)
+        recent_data = asset.returns[-observation_period:]
         values.append(recent_data)
 
     # Calculate covariance matrix from recent data values
     cov_matrix = np.cov(values)
     
-    return np.array(avg_returns), np.array(avg_stds), cov_matrix
+    return cov_matrix
+
+def calculate_sharpe_ratio(returns, risk, risk_free_rate=0.0):
+    """ Calculate the Sharpe ratio for given returns and risk. """
+    return (returns - risk_free_rate) / risk if risk != 0 else np.inf
+
+def calculate_penalty(weights, target_weights):
+    """ Calculate a penalty for deviation from target weights. """
+    deviation = np.sum((weights - target_weights) ** 2)
+    return np.sum(deviation ** 2)  # Non-linear penalty
 
 def fitness(weights, expected_returns, cov_matrix, capm_weights):
     """ Calculate the fitness of a portfolio configuration. """
     portfolio_return = np.dot(weights, expected_returns)
     portfolio_risk = np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights)))
-    sharpe_ratio = portfolio_return / portfolio_risk
-    capm_deviation = np.sum((weights - capm_weights) ** 2)
-    
-    # Enhance penalty non-linearly
-    penalty = np.sum(capm_deviation ** 2)
-    
+
+    sharpe_ratio = calculate_sharpe_ratio(portfolio_return, portfolio_risk)
+    penalty = calculate_penalty(weights, capm_weights)
+
     return sharpe_ratio - penalty,
